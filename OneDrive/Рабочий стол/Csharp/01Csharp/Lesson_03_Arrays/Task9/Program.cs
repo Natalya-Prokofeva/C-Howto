e90f4e84a9118657668fa581674dc1f06cf5e36e
Natalya Prokofeva <p.natoxa@yandex.ru>
2024-02-22 21:47:47 +0300

    add Lesson_03_Arrsys task 5,6,7,8,9

---
+++ OneDrive/Рабочий стол/Csharp/01Csharp/Lesson_03_Arrays/Task9/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] arr = new int [3];
+int num = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = num % 10;
+    num = num / 10;
+    Console.Write(arr[i] + " ");
+}
