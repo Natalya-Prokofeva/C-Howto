ed1b33efe88dd419b72ca2b60fad40d33902d361
Natalya Prokofeva <p.natoxa@yandex.ru>
2024-02-23 23:09:38 +0300

    add Lesson_03:dz1,dz2,dz3

--- OneDrive/Рабочий стол/Csharp/01Csharp/Lesson_03_Arrays/Dz1/Program.cs
+++ OneDrive/Рабочий стол/Csharp/01Csharp/Lesson_03_Arrays/Dz1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив из вещественных чисел с ненулевой дробной частью.
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] numbers= {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+
+double FindMin = numbers[0];
+double FindMax = numbers[0];
+double diff = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < FindMin)
+        {   
+        FindMin = numbers[i];
+        }
+    if (numbers[i] > FindMax)
+        {   
+        FindMax = numbers[i];
+        }
+}
+diff = FindMax - FindMin ;
+Console.Write(diff);
