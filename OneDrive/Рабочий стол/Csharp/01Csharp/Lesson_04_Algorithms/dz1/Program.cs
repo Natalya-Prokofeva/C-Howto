981847cfcce23c15f7ac00b69326ffb24c40383c
Natalya Prokofeva <p.natoxa@yandex.ru>
2024-02-23 17:37:08 +0300

    add Lesson_04:dz1.2.3.4

---
+++ OneDrive/Рабочий стол/Csharp/01Csharp/Lesson_04_Algorithms/dz1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 1: Напишите программу, 
+//которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+int num1 = int.Parse(Console.ReadLine()!);
+
+if(num1 % 7 == 0 && num1 % 23 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
